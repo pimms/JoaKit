bfa4b3b4e180d962b3fdbd699dac7848d91ba510
Joakim Stien <joakim.stien@gmail.com>
2019-11-08 13:15:17 +0100

    Initial Commit

---
+++ JoaKit/JoaKit.h
@@ -0,0 +1,19 @@
+//
+//  JoaKit.h
+//  JoaKit
+//
+//  Created by pimms on 08/11/2019.
+//  Copyright © 2019 Joakim Stien. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JoaKit.
+FOUNDATION_EXPORT double JoaKitVersionNumber;
+
+//! Project version string for JoaKit.
+FOUNDATION_EXPORT const unsigned char JoaKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JoaKit/PublicHeader.h>
+
+
