85a3ee52b0b92c2be43c8cae99286f6852dd00f9
Joakim Stien <joakim.stien@gmail.com>
2019-11-10 14:36:24 +0100

    Added UIView.removeAllConstraints()

--- JoaKit/Extensions/UIView.swift
+++ JoaKit/Extensions/UIView.swift
@@ -37,4 +37,26 @@ public extension UIView {
             bottomAnchor.constraint(equalTo: superview.bottomAnchor, constant: -insets.bottom),
         ])
     }
+
+    func removeAllConstraints() {
+        // All credits to marchinram @ StackOverflow:
+        // https://stackoverflow.com/a/30491911
+        var _superview = superview
+
+        while let superview = _superview {
+            for constraint in superview.constraints {
+                if let first = constraint.firstItem as? UIView, first == self {
+                    superview.removeConstraint(constraint)
+                }
+
+                if let second = constraint.secondItem as? UIView, second == self {
+                    superview.removeConstraint(constraint)
+                }
+            }
+
+            _superview = superview.superview
+        }
+
+        removeConstraints(constraints)
+    }
 }
